cadff0325e61d6edee1f48aaefffceb07feda085
huongnt <nguyen.thi.huong@sun-asterisk.com>
2024-02-07 09:53:39 +0700

    Part 4: Template primer, Home page

--- src/controller/book.controller.ts
+++ src/controller/book.controller.ts
@@ -3,6 +3,36 @@ import asyncHandler from 'express-async-handler'
 
 import { AppDataSource } from '../config/data-source'
 import { Book } from '../entity/book.entity'
+import { Author } from '../entity/author.entity'
+import { Genre } from '../entity/genre.entity'
+import { BookInstance } from '../entity/book_instance.entity'
+import { BookInstanceStatus } from '../enum/book_instance.enum'
+
+const bookRepository = AppDataSource.getRepository(Book)
+const authorRepository = AppDataSource.getRepository(Author)
+const genreRepository = AppDataSource.getRepository(Genre)
+const bookInstanceRepository = AppDataSource.getRepository(BookInstance)
+
+export const index = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const [numBooks, numBookInstances, availableBookInstances, numAuthors, numGenres] = await Promise.all([
+    bookRepository.count(),
+    bookInstanceRepository.count(),
+    bookInstanceRepository.findAndCount({
+      where: { status: BookInstanceStatus.Available }
+    }),
+    authorRepository.count(),
+    genreRepository.count()
+  ])
+
+  res.render('index', {
+    title: 'Local library home',
+    book_count: numBooks,
+    book_instance_count: numBookInstances,
+    book_instance_available_count: availableBookInstances[1],
+    author_count: numAuthors,
+    genre_count: numGenres
+  })
+})
 
 export const bookList = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   res.send('NOT IMPLEMENTED: book list')
